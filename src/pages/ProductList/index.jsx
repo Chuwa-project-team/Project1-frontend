87fb28135cba81a48b91c8ff36907f487b5ce6c1
Shuyuan <kukutou_mikiya@outlook.com>
2023-10-13 18:40:33 -0400

    feat: add order selection of product list

--- src/pages/ProductList/index.jsx
+++ src/pages/ProductList/index.jsx
@@ -5,19 +5,45 @@ import  {getAllProducts} from '../../services/product';
 import ProductItem from '../../components/ProductItem';
 import { useSelector, useDispatch } from 'react-redux';
 import { editCartProduct } from '../../app/cartSlice';
+import { Select } from 'antd';
+
 export default function ProductList() {
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
     const { isAuthenticated, user } = useSelector(state => state.user);
     const cartProducts = useSelector(state => state.cart.products);
-    const dispatch = useDispatch();
+
     const [products, setProducts] = useState([]);
-    const navigate = useNavigate();
+    const [order, setOrder] = useState('Last added');
+
 
     useEffect( () => {
         async function fetchProducts() {
           setProducts(await getAllProducts()) 
         }
         fetchProducts();
+        console.log(products)
     }, []);
+
+    useEffect(() => {
+      const sortedProducts = [...products];
+      if (order === 'Last added') {
+        sortedProducts.sort((a, b) => {
+          return new Date(b.createDate) - new Date(a.createDate);
+        });
+      } else if (order === 'Price: low to high') {
+        sortedProducts.sort((a, b) => {
+          return a.price - b.price;
+        });
+      }
+      else if (order === 'Price: high to low') {
+        sortedProducts.sort((a, b) => {
+          return b.price - a.price;
+        });
+      }
+      setProducts(sortedProducts);
+    }, [order]);
 
     const cartCountHandlerCreator = (product) => {
       return (value) => {
@@ -34,9 +60,33 @@ export default function ProductList() {
         navigate(`/editProduct/${product.name}`, { state: { product } });
     };
 
+    const naviToAddProduct = () => {
+        navigate(`/createProduct`);
+    }
+
+    const handleOrderChange = (value) => {
+      setOrder(value);
+    }
     return (
         <div>
-        <h2>ProductList</h2>
+        <Row gutter={16}>
+          < Col xs={24} md={12}>
+            Product List
+          </Col>
+          <Col xs={24} md={12}>
+          <Select
+            defaultValue="Last added"
+            style={{ width: 120 }}
+            onChange={handleOrderChange}
+            options={[
+              { value: 'Last added', label: 'Last added' },
+              { value: 'Price: low to high', label: 'Price: low to high' },
+              { value: 'Price: high to low', label: 'Price: high to low' },
+            ]}
+          />
+            <button onClick={naviToAddProduct}>Add Product</button>
+          </Col>
+        </Row>
         <Row gutter={16}> {/* Adjust gutter as needed */}
           {products.map(product => {
             const cart_product = cartProducts.find(p => p.product.name === product.name);
