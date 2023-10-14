56c1fb8827392b2e40def1d8f72464f8c40b1cbf
ruoyuc <ruoyuc@usc.edu>
2023-10-13 21:40:02 -0400

    Update index.jsx

--- src/pages/Product/index.jsx
+++ src/pages/Product/index.jsx
@@ -1,44 +1,45 @@
-import { useLocation } from 'react-router-dom';
-import { Button, Card, Typography, Tag } from 'antd';
-
+import "./styles.css";
+import { Button, Card, Typography, Tag,Flex} from 'antd';
+import Meta from './meta.png'
 const { Title, Text } = Typography;
-export default function Product() {
-    const location = useLocation();
-    const product = location.state.product;
-    console.log(product);
-    // Use product info to display details
 
-    return (
-        <div style={{ padding: '24px', display: 'flex', justifyContent: 'center' }}>
-          <Card
-            style={{ width: 800, display: 'flex', flexDirection: 'row' }}
-            bordered={false}
-          >
-            <img
-              src="./meta"
-              alt="Product"
-              style={{ width: '50%', objectFit: 'cover' }}
-            />
-    
-            <div style={{ paddingLeft: '24px', width: '50%' }}>
-              <Text type="secondary">Category1</Text>
-              <Title level={2}>Meta Quest2 VR headset</Title>
-              <Title level={3}>$299</Title>
-              <Tag color="red">Out of Stock</Tag>
-              <Text>
-                Hundreds of hit games, one-of-a-kind experiences, live events,
-                new ways to stay fit and a growing community... 
-              </Text>
-    
-              <div style={{ marginTop: '24px' }}>
-                <Button type="primary" style={{ marginRight: '16px' }}>
-                  Add To Cart
-                </Button>
-                <Button>Edit</Button>
-              </div>
-            </div>
-          </Card>
+function App() {
+  return (
+    <div style={{ padding: '24px', display: 'flex', justifyContent: 'center' }}>
+      <Card
+        style={{ width: 800 }}
+        bordered={true}
+      >
+        <Flex justify = "space-between" >
+        {/* wrap="wrap" gap="small" */}
+        <img
+          src={Meta}
+          alt="Product"
+          style={{ width: '40%', objectFit: 'cover' }}
+        />
+
+        <div style={{ paddingLeft: '24px', width: '50%' }}>
+          <Text type="secondary">Category1</Text>
+          <Title level={2}>Meta Quest2 VR headset</Title>
+          <Title level={3}>$299</Title>
+          <Tag color="red">Out of Stock</Tag>
+          <Text>
+            Hundreds of hit games, one-of-a-kind experiences, live events,
+            new ways to stay fit and a growing community... 
+          </Text>
+
+          <div style={{ marginTop: '24px' }}>
+            <Button type="primary" style={{ marginRight: '16px' }}>
+              Add To Cart
+            </Button>
+            <Button>Edit</Button>
+          </div>
         </div>
-      );
+        </Flex>
+      </Card>
+    </div>
+  );
 }
+
+export default App;
 
