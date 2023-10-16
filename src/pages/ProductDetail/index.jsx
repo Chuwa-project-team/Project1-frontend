e826535a29eff38eed6d543c5657c511f9f13a1a
ruoyuc <ruoyuc@usc.edu>
2023-10-16 18:52:59 -0400

    Update index.jsx

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -116,61 +116,6 @@ function ProductDetail() {
             </div>
           </div>
         </div>
-
-        {/* <div style={{ display: "flex", justifyContent: "space-between" }}>
-          <img
-            src={product.imageUrl}
-            alt={product.name}
-            style={{ width: "40%", objectFit: "cover" }}
-          />
-          <div style={{ paddingLeft: "24px", width: "50%" }}>
-            <Text type="secondary">{product.category}</Text>
-            <Title level={2}>{product.name}</Title>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              <Title level={3}>${product.price}</Title>
-              {product.quantity !== 0 ? (
-                <Tag
-                  color="green"
-                  style={{ height: "fit-content", marginLeft: "8px" }}
-                >
-                  In Stock
-                </Tag>
-              ) : (
-                <Tag
-                  color="red"
-                  style={{ height: "fit-content", marginLeft: "8px" }}
-                >
-                  Out of Stock
-                </Tag>
-              )}
-            </div>
-            <Text>{product.description}</Text>
-            <div style={{ marginTop: "24px" }}>
-              {user.role === "regular" || user.role === "admin" ? (
-                count === 0 ? (
-                  <Button
-                    type="primary"
-                    style={{ marginRight: "16px" }}
-                    onClick={() => countHandler(1)}
-                  >
-                    Add To Cart
-                  </Button>
-                ) : (
-                  <Counter handler={countHandler} value={count}></Counter>
-                )
-              ) : null}
-              {user.role === "admin" ? (
-                <Button
-                  onClick={() => {
-                    editHandler(product);
-                  }}
-                >
-                  Edit
-                </Button>
-              ) : null}
-            </div>
-          </div>
-        </div> */}
       </Card>
     </div>
   );
