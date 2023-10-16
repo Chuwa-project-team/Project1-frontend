3ce19cff62d0664210afc3519a7682b211b96168
ruoyuc <ruoyuc@usc.edu>
2023-10-15 22:35:51 -0400

    Update index.jsx

--- src/components/ProductItem/index.jsx
+++ src/components/ProductItem/index.jsx
@@ -1,55 +1,67 @@
 /* eslint-disable no-unused-vars */
 
 /* eslint-disable react/prop-types */
-import   Counter  from '../Counter';
-import placeholderImage from '../../assets/place_holder.jpg';
-import { InputNumber } from 'antd';
-import { Link } from 'react-router-dom';
-import styles from './productItem.module.css';
-export default function ProductItem ({
-    name,
-    imageUrl,
-    price,
-    description,
-    count,
-    countHandler,
-    editHandler,
-    userRole
+import Counter from "../Counter";
+import placeholderImage from "../../assets/place_holder.jpg";
+import { InputNumber } from "antd";
+import { Link } from "react-router-dom";
+import styles from "./productItem.module.css";
+export default function ProductItem({
+  name,
+  imageUrl,
+  price,
+  description,
+  count,
+  countHandler,
+  editHandler,
+  userRole,
 }) {
-
-    return(
-        <>
-        <Link to={{pathname: `/product/${name}`,
-        state: {
-        product: {
-            name,
-            imageUrl,
-            price,
-            description,
-            count,
-            countHandler,
-            editHandler,
-            userRole
-                }
-                }
-            }}>
-            <img src={placeholderImage} alt="Placeholder" width={300} height={200} />
-        </Link>
-
-        <br/>
-        {name}
-        <br/>
-        ${price}
-        <br/>
-        {description}
-        <br/>
-        {count === 0?(
-            <button onClick={countHandler(1)}>Add</button>           
-             ):(
-            <InputNumber addonBefore={<p onClick={countHandler(1)}>+1</p>} addonAfter={<p onClick={countHandler(-1)}>-1</p>} defaultValue={0}></InputNumber>)
-        }  
-        {userRole === "admin"?(<button onClick={editHandler}>Edit</button>):(<button onClick={editHandler}>Edit</button>)
-        }  
-        </>
-    )
+  return (
+    <>
+      <Link
+        to={{
+          pathname: `/product/${name}`,
+          state: {
+            product: {
+              name,
+              imageUrl,
+              price,
+              description,
+              count,
+              countHandler,
+              editHandler,
+              userRole,
+            },
+          },
+        }}
+      >
+        <img
+          src={placeholderImage}
+          alt="Placeholder"
+          width={300}
+          height={200}
+        />
+      </Link>
+      <br />
+      {name}
+      <br />${price}
+      <br />
+      {description}
+      <br />
+      {count === 0 ? (
+        <button onClick={() => countHandler(1)}>Add</button>
+      ) : (
+        <InputNumber
+          addonBefore={<p onClick={() => countHandler(1)}>+1</p>}
+          addonAfter={<p onClick={() => countHandler(-1)}>-1</p>}
+          defaultValue={0}
+        />
+      )}
+      {userRole === "admin" ? (
+        <button onClick={editHandler}>Edit</button>
+      ) : (<></>
+        // <button onClick={editHandler}>Edit</button>
+      )}
+    </>
+  );
 }
